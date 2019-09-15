ef2f92ec11e6ed9b2b1f49622a866b70414e70d1
sasano5452 <arukara5452@gmail.com>
2019-09-15 19:38:29 +0900

    ログイン等々

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #ログイン済みのuserのみアクセスを許可します
+  before_action :authenticate_user!
+
+  #deviseで利用できるパラメータの設定
+  before_action :configure_permitted_parameters, if: :devise_controller?  
+
+  protected
+
+  #サインアップ時nameも要求します
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end  
+
 end
