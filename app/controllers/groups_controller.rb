c3a6592adb3ba086845f80f23763f9fe9af12347
sasano5452 <arukara5452@gmail.com>
2019-09-16 10:02:45 +0900

    メインページに

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
+  def index
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -17,12 +20,21 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end  
   end
 
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => []})
   end  
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
   
 
 end
