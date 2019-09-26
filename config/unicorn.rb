06951e09eadb6da133155f7615c379a64053cfbd
sasano5452 <arukara5452@gmail.com>
2019-09-26 11:54:45 +0900

    ユニコーン

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,12 +2,17 @@ app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 1
 
+app_path = File.expand_path('../../../', __FILE__)
+
+worker_processes 1
+# currentを指定
 working_directory "#{app_path}/current"
+
+# それぞれ、sharedの中を参照するよう変更
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
-
-listen "#{app_path}/tmp/sockets/unicorn.sock"
 timeout 60
 
 preload_app true
